8fed3f325e5afed8f0aca3a0e6b4725f20f3f2ca
bagusrizky15 <mohamad.15@students.amikom.ac.id>
2023-07-07 09:04:38 +0700

    update

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageViewOne.image = UIImage(named: "DiceSix")
+        
     }
-
+    @IBAction func didClickRoll(_ sender: UIButton) {
+    }
+    
 
 }
 
